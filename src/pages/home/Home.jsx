e3ee08c4f0faf4b765f7405a7db56d5d38a21fc0
Saroj Rokaya <rokayasaroj188@gmail.com>
2024-09-07 23:39:00 +0545

    fetch data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar/Navbar.jsx'
 import Card from './Card.jsx'
 import Footer from '../../components/navbar/Footer.jsx'
+import axios from 'axios'
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+  const fetchProduct = async () => {
+    const response = await axios.get('https://66dc84a947d749b72acbc6f7.mockapi.io/products')
+    if (response.status === 200) {
+      setProducts(response.data)
+    }
+  }
+  useEffect(() => {
+    fetchProduct()
+  }, [])
+  console.log(products)
+
   return (
     <>
       <Navbar />
@@ -11,9 +24,14 @@ const Home = () => {
         <div className="container mx-auto px-4 py-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 
-            <Card />
-            <Card />
-            <Card />
+            {
+              products.map((product)=>{
+                return (
+                  <Card product={product} />
+                )
+              })
+            }
+
           </div>
         </div>
       </div>
